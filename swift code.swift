fd1057c3b4833a7cdf587f7f9c339ee63589b5b1
Chen <gh1321223@gmail.com>
2020-03-15 13:42:53 +0800

    first try

---
+++ swift code.swift
@@ -0,0 +1,16 @@
+print("hello swift")
+print("second line")
+// this is comment
+
+/*  
+this is
+multiple line comment
+and there are 
+Int  Double  String  Bool
+four types of variable
+*/
+
+var year = 2020 //variable and output
+print(year)
+
+var price = 18.70

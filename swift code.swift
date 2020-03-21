49023de702acd64b7b015e75e28c65dcc9601167
Chen <gh1321223@gmail.com>
2020-03-21 18:24:42 +0800

    by Chen in vs code at 0321

--- swift code.swift
+++ swift code.swift
@@ -20,11 +20,34 @@ print(count)
 
 count /= 1.25
 print(count)
-// declare variable use -> var year: Int 
+// declare variable -> var year: Int 
+// #need upper case
 var age: Int = 24
 print("I am \(age) years old.") // this is the string interpolation
 
+// temperture transfer
 var tempf: Double = 56
 var tempc: Double 
 tempc = (tempf-32)/1.8
 print ("The temp is \(tempc) degrees Celsius.")
+
+// BMI calculation
+var height: Double = 1.76
+var weight: Double = 78
+var bmi: Double 
+bmi = weight/(height*height)
+print("Your BMI is \(bmi).")
+
+/* up above is the basic code of swift 
+Next will be more amd more fun :) */
+
+//if else statement
+var learningToCode = true
+
+//learningToCode = false
+
+if learningToCode {
+  print("Don't forget to take breaks! You got this 💪")
+} else {
+    print("remember to improve!!")
+}
